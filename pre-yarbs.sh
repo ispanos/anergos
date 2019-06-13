7c4c12941b6ec8b7b651c1b158eb4d2764c9d1e3
ispanos <yiannisvspanos@gmail.com>
2019-06-13 23:53:21 +0300

    test failed || I think its fixed now

--- pre-yarbs.sh
+++ pre-yarbs.sh
@@ -45,10 +45,8 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 
 curl -sL "https://raw.githubusercontent.com/ispanos/YARBS/master/yarbs.sh" > /mnt/yarbs.sh && \
-arch-chroot /mnt bash /tmp/yarbs.sh && \
+arch-chroot /mnt bash yarbs.sh && \
 rm /mnt/yarbs.sh || \
 printf "\\n\\n\\n\\n\\n\\n\\n\\nsomething went wrong." && clear && exit
 
-arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh
 dialog --defaultno --yesno "Reboot computer?"  5 30 && reboot
-dialog --defaultno --yesno "Return to chroot environment?" 6 30 && arch-chroot /mnt
