3bb51131bbe05fb105cd2bf6b4fe25d5e51729f2
ispanos <yiannisvspanos@gmail.com>
2019-06-18 17:56:53 +0300

    added harddrive selection

--- pre-yarbs.sh
+++ pre-yarbs.sh
@@ -5,10 +5,23 @@
 # bash pre-yarbs
 
 pacman -Sy --needed --noconfirm dialog # termite-terminfo
-
 timedatectl set-ntp true
 
-HARD_DRIVE="/dev/sda"
+
+
+drive_list_vert=$(/usr/bin/ls -1 /dev | grep "sd.$" && /usr/bin/ls -1 /dev | grep "nvme.$")
+
+list_hard_drives(){
+    # All mounted partitions in one line, numbered, separated by a space to make the menu list for dialog
+    for i in $drive_list_vert ; do
+        local -i n+=1
+        printf " $n $i"
+    done
+}
+
+hard_drive_num=$(dialog --title "Select your Hard-drive" --menu "$(lsblk)" 0 0 0 $(list_hard_drives) 3>&1 1>&2 2>&3 3>&1)
+
+HARD_DRIVE="/dev/"$( echo $drive_list_vert | tr " " "\n" | sed -n ${hard_drive_num}p)
 ESP_path="/boot"
 
 umount ${HARD_DRIVE}* 2>/dev/null
