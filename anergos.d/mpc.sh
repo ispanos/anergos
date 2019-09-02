2ba63fc435b72bb79f71dd92cc6e6c64fc22d0b0
ispanos <yiannisvspanos@gmail.com>
2019-09-03 00:22:58 +0300

    more uniform

--- anergos.d/mpc.sh
+++ anergos.d/mpc.sh
@@ -2,32 +2,41 @@
 
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/ispanos/dotfiles.git"
 
+function chech_val() {
+	printf $(tput setaf 3)"${FUNCNAME[1]}....\t\t - "$(tput sgr0)
+	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"skipped"$(tput sgr0)
+}
+
+function ready() {
+ echo $(tput setaf 2)"done"$@$(tput sgr0)
+}
+
 function nobeep() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function power_group() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	gpasswd -a $name power >/dev/null 2>&1
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function all_core_make() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	grep "^MAKEFLAGS" /etc/makepkg.conf >/dev/null 2>&1 &&
-	echo $(tput setaf 1)"${FUNCNAME[0]} failed - '^MAKEFLAGS' exists"$(tput sgr0) && return
+	ready "- '^MAKEFLAGS' exists" && return
 	sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function networkd_config() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 
 	if [ -f  /usr/bin/NetworkManager ]; then
 		systemctl enable NetworkManager >/dev/null 2>&1
-		echo $(tput setaf 2)"NetworkManager done"$(tput sgr0)
+		ready " (NetworkManager)"
 
 	else
 		net_lot=$(networkctl --no-legend 2>/dev/null | grep -P "ether|wlan" | awk '{print $2}')
@@ -43,69 +52,67 @@ function networkd_config() {
 		done
 		systemctl enable systemd-networkd >/dev/null 2>&1
 		systemctl enable systemd-resolved >/dev/null 2>&1
-		echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+		ready
 	fi 
 	
 }
 
 function nano_configs() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	grep '^include "/usr/share/nano/*.nanorc"' /etc/nanorc >/dev/null 2>&1 || 
 	echo 'include "/usr/share/nano/*.nanorc"' >> /etc/nanorc
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function infinality(){
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	sed -i 's/^#exp/exp/;s/version=40"$/version=38"$/' /etc/profile.d/freetype2.sh
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function office_logo() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	[ -f /etc/libreoffice/sofficerc ] || 
-	echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val 0 && return
 	sed -i 's/Logo=1/Logo=0/g' /etc/libreoffice/sofficerc	
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function create_swapfile() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	fallocate -l 2G /swapfile 	>/dev/null 2>&1
 	chmod 600 /swapfile
 	mkswap /swapfile 			>/dev/null 2>&1
 	swapon /swapfile 			>/dev/null 2>&1
 	printf "# Swapfile\\n/swapfile none swap defaults 0 0\\n\\n" >> /etc/fstab
 	printf "vm.swappiness=10\nvm.vfs_cache_pressure=50\n" > /etc/sysctl.d/99-sysctl.conf
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function clone_dotfiles() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	cd /home/"$name" && echo ".cfg" >> .gitignore && rm .bash_profile .bashrc
 	sudo -u "$name" git clone --bare "$dotfilesrepo" /home/${name}/.cfg > /dev/null 2>&1 
 	sudo -u "$name" git --git-dir=/home/${name}/.cfg/ --work-tree=/home/${name} checkout
 	sudo -u "$name" git --git-dir=/home/${name}/.cfg/ --work-tree=/home/${name} config \
 				--local status.showUntrackedFiles no > /dev/null 2>&1 && rm .gitignore
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function arduino_groups() {
-	if [ ! -f /usr/bin/arduino ] && [ $1 ] && [ $1 -eq 0 ]; then
-		echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
-	fi
-
+	chech_val && return
+	[ ! -f /usr/bin/arduino ] && chech_val 0 && return
 	echo cdc_acm > /etc/modules-load.d/cdc_acm.conf
 	sudo -u "$name" groups | grep uucp >/dev/null 2>&1 || gpasswd -a $name uucp >/dev/null 2>&1
 	sudo -u "$name" groups | grep lock >/dev/null 2>&1 || gpasswd -a $name lock >/dev/null 2>&1
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function agetty_set() {
 	systemctl enable gdm >/dev/null 2>&1 && 
-		echo $(tput setaf 2)"GDM done (value $1)"$(tput sgr0) && return
+		ready " GDM (value $1)" && return
 	
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	if [ "$1" = "auto" ]; then
 		local log="ExecStart=-\/sbin\/agetty --autologin $name --noclear %I \$TERM"
 	else
@@ -113,13 +120,12 @@ function agetty_set() {
 	fi
 	sed "s/ExecStart=.*/${log}/" /usr/lib/systemd/system/getty@.service > \
 								/etc/systemd/system/getty@.service
-	systemctl daemon-reload 				>/dev/null 2>&1
-	systemctl reenable getty@tty1.service 	>/dev/null 2>&1
-	echo $(tput setaf 2)"${FUNCNAME[0]} done (value $1)"$(tput sgr0)
+	systemctl daemon-reload >/dev/null 2>&1; systemctl reenable getty@tty1.service >/dev/null 2>&1
+	ready " (value $1)"
 }
 
 function lock_sleep() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	if [ -f /usr/bin/i3lock ] && [ ! -f /usr/bin/sway ]; then
 		cat > /etc/systemd/system/SleepLocki3@${name}.service <<-EOF
 			#/etc/systemd/system/
@@ -137,44 +143,44 @@ function lock_sleep() {
 		EOF
 	fi
 	systemctl enable SleepLocki3@${name} >/dev/null 2>&1
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function virtualbox() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 
 	if [[ $(lspci | grep VirtualBox) ]]; then
-		if [ -f /usr/bin/pacman ]; then
+		if hostnamectl | grep -q "Arch Linux"; then
 			local g_utils="virtualbox-guest-modules-arch virtualbox-guest-utils xf86-video-vmware"
 			pacman -S --noconfirm $g_utils >/dev/null 2>&1
 
 			if [ -f /usr/bin/virtualbox ]; then
+				printf "Removing VirtualBox... "
 				pacman -Rns --noconfirm virtualbox >/dev/null 2>&1
 				pacman -Rns --noconfirm virtualbox-host-modules-arch >/dev/null 2>&1
 				pacman -Rns --noconfirm virtualbox-guest-iso >/dev/null 2>&1 
-				echo "Removed VirtualBox. This is a virtualbox guest"
 			fi
-			echo $(tput setaf 2)"${FUNCNAME[0]}-guest done"$(tput sgr0)
+			ready " - guest"
 		else
-			echo $(tput setaf 1)"${FUNCNAME[0]} skipped (pacman is not installed)"$(tput sgr0)
+			echo $(tput setaf 1)"- Guest is not ArchLinux"$(tput sgr0)
 		fi
 
 
 	elif [ -f /usr/bin/virtualbox ]; then
 		sudo -u "$name" groups | grep vboxusers >/dev/null 2>&1 || 
 			gpasswd -a $name vboxusers >/dev/null 2>&1
-		echo $(tput setaf 2)"${FUNCNAME[0]}-host done"$(tput sgr0)
+		ready " - host"
 	fi
 }
 
 function resolv_conf() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	printf "search home\\nnameserver 192.168.1.1\\n" > /etc/resolv.conf
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function enable_numlk_tty() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	cat > /etc/systemd/system/numLockOnTty.service <<-EOF
 		[Unit]
 		Description=numlockOnTty
@@ -194,33 +200,33 @@ function enable_numlk_tty() {
 	EOF
 	chmod +x /usr/bin/numlockOnTty
 	systemctl enable numLockOnTty >/dev/null 2>&1
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function temps() { # https://aur.archlinux.org/packages/it87-dkms-git || github.com/bbqlinux/it87
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
-	[ ! -f /usr/bin/yay ] && return
+	chech_val $1 && return
+	[ ! -f /usr/bin/yay ] && chech_val 0 && return
 	sudo -u "$name" yay -S --noconfirm it87-dkms-git >/dev/null 2>&1
 	echo "it87" > /etc/modules-load.d/it87.conf
 	echo "it87-dkms-git" >> /home/"$name"/.local/Fresh_pack_list
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function data() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	mkdir -p /media/Data
 	cat >> /etc/fstab <<-EOF
 		# /dev/sda1 LABEL=data
 		UUID=fe8b7dcf-3bae-4441-a4f3-a3111fee8ca4 /media/Data ext4 rw,noatime,nofail,user,auto 0 2
 	
 	EOF
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function powerb_is_suspend() {
-	[ $1 ] && [ $1 -eq 0 ] && echo $(tput setaf 1)"${FUNCNAME[0]} skipped"$(tput sgr0) && return
+	chech_val $1 && return
 	sed -i "s/^#HandlePowerKey=poweroff/HandlePowerKey=suspend/g" /etc/systemd/logind.conf
-	echo $(tput setaf 2)"${FUNCNAME[0]} done"$(tput sgr0)
+	ready
 }
 
 function set_sane_permitions() {
@@ -231,7 +237,7 @@ cat > /etc/sudoers.d/wheel <<-EOF
 /usr/bin/systemctl restart NetworkManager
 EOF
 chmod 440 /etc/sudoers.d/wheel
-echo $(tput setaf 2)"${FUNCNAME[0]} in $0 Done!"$(tput sgr0)
+echo $(tput setaf 2)"${FUNCNAME[0]}- in $0 Done!"$(tput sgr0)
 sleep 15
 }
 
