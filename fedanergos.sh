4a225847084a3d0ca18b446e3194d58d9954945e
ispanos <yiannisvspanos@gmail.com>
2019-11-14 18:43:37 +0200

    fedora stuff

--- fedanergos.sh
+++ fedanergos.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 # License: GNU GPLv3
 
+if [ $(id -u) = 0 ]; then
+   echo "This script changes your user folder and should not be run as root!"
+   echo "You may need to enter your password multiple times!"
+   exit 1
+fi
+
+
+# System
+source common.sh
+agetty_set; i3lock_sleep; it87_driver; data;
+# Chages the power-button on the pc to a sleep button.
+sudo sed -i '/HandlePowerKey/{s/=.*$/=suspend/;s/^#//}' /etc/systemd/logind.conf
+
 [ -d ~/.local ] && mkdir ~/.local
 dnf list installed > ~/.local/Freshiest
 sudo dnf clean all
@@ -10,98 +23,19 @@ sudo dnf upgrade -y
 
 sudo dnf copr enable skidnik/i3blocks -y
 
+sudo dnf install -y \
+flatpak \
+$(cat programs/fedora.i3.csv | sed '/^#/d;/^,/d;s/,.*$//' | tr "\n" " ")
+sudo dnf remove -y openssh-server
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-#cat programs/fedora.i3.csv | grep -vP "^#|^,"  | awk -F, '{print $1 " \\"}'
-
-sudo dnf install \
--y \
-atool \
-calcurse \
-exfat-utils \
-git \
-fzf \
-highlight \
-htop \
-lsd \
-lshw \
-mediainfo \
-neofetch \
-neovim \
-nmap \
-ntfs-3g \
-odt2txt \
-p7zip \
-sshfs \
-tmux \
-ufw \
-unrar \
-vifm \
-w3m \
-wpa_supplicant \
-youtube-dl \
-inxi \
-glxinfo \
-simple-mtpfs \
-i3 \
-i3blocks \
-jq \
-xorg-x11-server-Xorg \
-xorg-x11-xinit \
-xorg-x11-server-utils \
-xbacklight \
-compton \
-xdotool \
-dunst \
-numlockx \
-feh \
-maim \
-xkb-switch \
-libnotify \
-gnome-terminal \
-playerctl \
-galculator \
-pcmanfm \
-gvfs-afc \
-gvfs-gphoto2 \
-gvfs-mtp \
-gvfs-smb \
-xarchiver \
-firefox \
-hunspell-en \
-hunspell-el \
-NetworkManager \
-nm-connection-editor \
-NetworkManager-openconnect \
-NetworkManager-openvpn \
-NetworkManager-pptp \
-NetworkManager-vpnc \
-NetworkManager-ssh \
-paprefs \
-pavucontrol \
-polkit-gnome \
-liberation-fonts \
-gcolor3 \
-mpv \
-zathura-pdf-poppler \
-qt5ct \
-transmission-gtk \
-libreoffice \
-libreoffice-langpack-el \
-libreoffice-langpack-en
-
-sudo dnf remove \
--y \
-openssh-server
 
 dnf list installed > ~/.local/Fresh
 
-source common.sh
-    clone_dotfiles  https://github.com/ispanos/dotfiles
-    firefox_configs https://github.com/ispanos/mozzila
-    agetty_set
-    i3lock_sleep
-    it87_driver
-    data
-    power_to_sleep
 
-[ -f /usr/bin/docker ] && gpasswd -a $name docker >/dev/null 2>&1
+# User settings.
+[ -f /usr/bin/docker ] && sudo usermod -aG docker $USER
+# Install steam.
+# flatpak -y install flathub com.valvesoftware.Steam
+clone_dotfiles  https://github.com/ispanos/dotfiles
+firefox_configs https://github.com/ispanos/mozzila
